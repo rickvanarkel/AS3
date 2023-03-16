d84bd7fc09e0d64cb665b432cf4da90159bc3f9c
Daan de Jager <daan@jagernet.nl>
2023-03-16 17:07:46 +0100

    nETWORKX MODEL

--- EPA1352-G11-A3/model/NetworkX.py
+++ EPA1352-G11-A3/model/NetworkX.py
@@ -5,29 +5,56 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def add_edge_to_graph(G, e1, e2, w):
     G.add_edge(e1, e2, weight=w)
+def make_points_edges(df):
+    road_dict = {}
+    roads = df['road'].unique()
+    point_num = 0
+    total_length = 0
 
-G = nx.Graph()
+    for road_type in roads:
+        temp_df =  df.loc[df['road'] == road_type]
+        temp_points = []
+        temp_edges = []
+        df_length = temp_df.shape[0]
+
+        for index, row in temp_df.iterrows():
+            temp_points.append((row['lon'], row['lat']))
+            if point_num != 0 and point_num != total_length:
+                temp_edges.append((point_num-1, point_num, row['length']))
+            point_num += 1
+        road_dict[road_type] = (temp_points, temp_edges)
+        total_length += df_length
+    return road_dict
+
+def make_networkx(road_dict):
+    G = nx.Graph()
+    edges_num = 0
+
+    for roads in df['road'].unique():
+        temp_list = road_dict[roads]
+
+        points = temp_list[0]
+        edges = temp_list[1]
+        edges_num += len(edges)
+
+        for i in range(len(edges)):
+            add_edge_to_graph(G, points[edges[i][0]-edges[0][0]], points[edges[i][1]-edges[0][0]], edges[i][2])
+
+    pos = nx.spring_layout(G)
+    nx.draw_networkx(G, pos=pos, node_color='k')
+    nx.draw_networkx(G, pos=pos, node_size=10)  # draw nodes and edges
+    #nx.draw_networkx_labels(G, pos=pos)  # draw node labels/names
+
+    # draw edge weights
+    labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+    #plt.axis()
+    plt.show()
+
 file_name = '../data/demo-4.csv'
 df = pd.read_csv(file_name)
 
-for index, row in df.iterrows():
-    print(row['lon'], row['lat'], row['length'])
-
-#
-# points = [(1, 10), (8, 10), (10, 8), (7, 4), (3, 1)]  # (x,y) points
-# edges = [(0, 1, 10), (1, 2, 5), (2, 3, 25), (0, 3, 3), (3, 4, 8)]  # (v1,v2, weight)
-# for i in range(len(edges)):
-#     add_edge_to_graph(G, points[edges[i][0]], points[edges[i][1]], edges[i][2])
-#
-# pos = nx.spring_layout(G)
-# nx.draw(G, pos=pos, node_color='k')
-# nx.draw(G, pos=pos, node_size=1500)  # draw nodes and edges
-# nx.draw_networkx_labels(G, pos=pos)  # draw node labels/names
-# # draw edge weights
-# labels = nx.get_edge_attributes(G, 'weight')
-# nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
-# plt.axis()
-# plt.show()
+road_dict = make_points_edges(df)
+make_networkx(road_dict)
